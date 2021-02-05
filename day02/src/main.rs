32d223e781e2029dc1243fa18e7398f2e8c415cf
marxin <mliska@suse.cz>
2021-02-05 20:34:58 +0100

    Finish part 2.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,15 +1,13 @@
 use std::fs;
 
-fn position_to_buton(position: &(i32, i32)) -> i32 {
-    7 - 3 * position.1 + position.0
-}
-
 fn main() {
     let input = fs::read_to_string("input.txt")
         .expect("Could not open input file");
-    let mut position = (1, 1);
-    let moves = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+    let mut position:(i32, i32) = (0, 2);
+    let moves:[(i32, i32);4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
     let move_names = "URDL";
+    // let keypad = ["123", "456", "789"];
+    let keypad = ["__1__", "_234_", "56789", "_ABC_", "__D__"];
 
     for line in input.lines() {
         // println!("Line: {}", line);
@@ -21,12 +19,19 @@ fn main() {
             position.0 += m.0;
             position.1 += m.1;
 
-            if position.0 > 2 || position.0 < 0 || position.1 > 2 || position.1 < 0 {
+            if position.0 >= keypad[0].len() as i32 || position.0 < 0 || position.1 >= keypad.len() as i32 || position.1 < 0 {
+                position = previous;
+            }
+
+            let key = keypad[position.1 as usize].chars().nth(position.0 as usize).unwrap();
+            if key == '_' {
                 position = previous;
             }
         }
 
-        println!("{:?}: {}", position, position_to_buton(&position));
+        let key = keypad[position.1 as usize].chars().nth(position.0 as usize).unwrap();
+
+        println!("{:?}: {}", position, key);
     }
 
     println!("Hello, world!");
